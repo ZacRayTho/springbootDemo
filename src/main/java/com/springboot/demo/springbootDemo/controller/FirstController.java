62a6858601f68ae033417792ea53a14bdf7f242b
zacraytho <zacraytho@gmail.com>
2025-03-24 20:29:24 -0400

    adding custom properties and expsoing a new brnach using the properties

--- src/main/java/com/springboot/demo/springbootDemo/controller/FirstController.java
+++ src/main/java/com/springboot/demo/springbootDemo/controller/FirstController.java
@@ -1,10 +1,22 @@
 package com.springboot.demo.springbootDemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
